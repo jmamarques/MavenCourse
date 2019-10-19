0c9b8bcff6a2855ad33dbdc1e492e6f8bad6b60e
Joao Marques <jm.a.marques.93@gmail.com>
2019-10-19 08:42:48 +0100

    add automatic maven jar plugin add simple test by squareTest add target folder to git ignore closes #4

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+public class HelloWorldTest {
+
+    void testMain() {
+        // Setup
+        final String[] args = new String[]{};
+
+        // Run the test
+        HelloWorld.main(args);
+
+        // Verify the results
+    }
+}
